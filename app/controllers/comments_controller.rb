49f8d55c48473d8e87e042042790e0cd93f17779
sert <demo.sert@gmail.com>
2016-07-20 01:39:27 +0900

    fixup comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
   def create
     article = Article.find(params[:article_id])
     comment = article.comments.create comment_params
+    comment.user = current_user
+    comment.save
     redirect_to article_path(article)
   end
 
