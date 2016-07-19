427aa37ca31eddf3fe957b1059a3471b839e294b
sert <demo.sert@gmail.com>
2016-07-20 00:59:46 +0900

    run 'rails g migration RemoveAuthorFromArticles author:string'

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719140705) do
+ActiveRecord::Schema.define(version: 20160719155530) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
-    t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
